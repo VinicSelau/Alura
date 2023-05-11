0df1e125e0ac83563b1cc567592926668b161470
Vinicius Selau da Silva <85204902+VinicSelau@users.noreply.github.com>
2023-05-11 17:50:13 -0300

    Add files via upload

--- Python/Games/jogos.py
+++ Python/Games/jogos.py
@@ -1,3 +1,6 @@
+import adivinhacao
+import forca
+
 print("*********************************")
 print("       Escolha o seu jogo!       ")
 print("*********************************")
@@ -8,9 +11,7 @@ jogo = int(input("Qual jogo?"))
 
 if(jogo == 1):
     print("Jogando Adivinhação")
-    import adivinhacao
     adivinhacao.jogar()
 elif(jogo == 2):
     print("Jogando Forca")
-    import forca
     forca.jogar()
